c49934ea72094492a7fe6cb257311fe49baeefce
Himanshu Sahu <himanshu.sahu@lyearn.com>
2020-12-12 21:51:43 +0530

    add hover styles to sidebar

--- src/components/SideBar/style.ts
+++ src/components/SideBar/style.ts
@@ -13,6 +13,11 @@ export const sidebarRootStyles = css`
     padding: 8px 18px 15px 15px;
   }
 
+  a:hover {
+    background-color: #f8f9fd;
+    border-radius: 0.5em/0.5em;
+  }
+
   span {
     padding: 0px 10px;
   }
@@ -27,7 +32,10 @@ export const topNavLinksStyles = css`
   height: 300px;
 `;
 
-export const bottomNavLinksStyles = css``;
+export const bottomNavLinksStyles = css`
+  display: flex;
+  flex-direction: column;
+`;
 
 export const iconStyles = css`
   position: relative;
@@ -37,7 +45,7 @@ export const iconStyles = css`
 export const selectedNavLink = css`
   background-color: #f8f9fd;
   font-weight: bold;
-  border-radius: 0.8em/0.7em;
+  border-radius: 0.6em/0.5em;
 
   span {
     color: #484c67;
